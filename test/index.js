8bbced6bcc7930d1f85ab7a0c58af6692dbe2cef
Kenan Yildirim <kenan@kenany.me>
2015-01-21 08:08:21 -0500

    Add another test

--- test/index.js
+++ test/index.js
@@ -8,6 +8,7 @@ test('exports a function', function(t) {
 });
 
 test('calculates binomial probability', function(t) {
-  t.plan(1);
+  t.plan(2);
   t.equal(pmf(2, 3, 0.7), 0.441);
+  t.equal(pmf(7, 10, 0.7), 0.26682793199999993);
 });
