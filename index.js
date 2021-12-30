9b0fed072285f86bdf90eedc22d53d6b1cbfca9f
Kenan Yildirim <kenan@kenany.me>
2021-12-30 17:50:51 -0500

    chore(deps): tape@5.4.0
    Closes https://github.com/KenanY/binomial-pmf/pull/68

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 var binomial = require('choose');
 
+/**
+ * @param {number} k
+ * @param {number} n
+ * @param {number} p
+ * @returns {number}
+ */
 function pmf(k, n, p) {
   return binomial(n, k) * Math.pow(p, k) * Math.pow(1 - p, n - k);
 }
